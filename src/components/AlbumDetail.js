5927697c1ede2842e6d4e1ef9cb940785f63cb09
Sam Thomson <samt@samt.st>
2017-12-08 13:13:20 +0700

    album covers and scrollable view

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,22 +4,39 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-    const { title, artist, thumbnail_image } = album;
-    const { thumbnailStyle, headerContentStyle } = styles;
+    const {
+        title,
+        artist,
+        thumbnail_image,
+        image
+    } = album;
+    const {
+        thumbnailStyle,
+        headerContentStyle,
+        thumbnailContainerStyle,
+        headerTextStyle,
+        albumImageStyle
+    } = styles;
 
     return (
         <Card>
             <CardSection>
-                <View>
+                <View style={thumbnailContainerStyle}>
                     <Image
                         style={thumbnailStyle}
                         source={{ uri: thumbnail_image }}
                     />
                 </View>
                 <View style={headerContentStyle}>
-                    <Text>{title}</Text>
+                    <Text style={headerTextStyle}>{title}</Text>
                     <Text>{artist}</Text>
                 </View>
+            </CardSection>
+            <CardSection>
+                <Image
+                    style={albumImageStyle}
+                    source={{ uri: image }}
+                />
             </CardSection>
         </Card>
     );
@@ -30,9 +47,23 @@ const styles = {
         flexDirection: 'column',
         justifyContent: 'space-around'
     },
+    headerTextStyle: {
+        fontSize: 18
+    },
     thumbnailStyle: {
         height: 50,
         width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    albumImageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
     }
 };
 
